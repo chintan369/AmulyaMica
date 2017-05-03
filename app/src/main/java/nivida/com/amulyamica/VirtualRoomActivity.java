eeebaf340620dc233643512338e04fffd2aaabe0
chintan369 <chintak369@gmail.com>
2017-05-03 19:07:33 +0530

    Virtualization Refresh Menu

--- app/src/main/java/nivida/com/amulyamica/VirtualRoomActivity.java
+++ app/src/main/java/nivida/com/amulyamica/VirtualRoomActivity.java
@@ -9,8 +9,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -48,6 +52,10 @@ public class VirtualRoomActivity extends AppCompatActivity {
 
         web_eGallery.getSettings().setLoadsImagesAutomatically(true);
         web_eGallery.getSettings().setJavaScriptEnabled(true);
+        web_eGallery.setWillNotCacheDrawing(true);
+        web_eGallery.destroyDrawingCache();
+        web_eGallery.getSettings().setAppCacheEnabled(false);
+        web_eGallery.getSettings().setBuiltInZoomControls(true);
         web_eGallery.getSettings().setAllowContentAccess(true);
         web_eGallery.getSettings().setAllowFileAccess(true);
         web_eGallery.getSettings().setAllowFileAccessFromFileURLs(true);
@@ -55,8 +63,14 @@ public class VirtualRoomActivity extends AppCompatActivity {
         web_eGallery.getSettings().setLoadWithOverviewMode(true);
         web_eGallery.getSettings().setDomStorageEnabled(true);
         web_eGallery.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        web_eGallery.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
 
-        web_eGallery.loadUrl("http://app.nivida.in/amulya_visualizer/");
+        web_eGallery.setWebChromeClient(new WebChromeClient(){
+
+        });
+
+        //web_eGallery.loadUrl("http://app.nivida.in/amulya_visualizer/");
+        web_eGallery.loadDataWithBaseURL("http://app.nivida.in/amulya_visualizer/",null,"text/html","UTF-8",null);
     }
 
     private void setActionBar() {
@@ -84,6 +98,22 @@ public class VirtualRoomActivity extends AppCompatActivity {
 
         mActionBar.setCustomView(mCustomView);
         mActionBar.setDisplayShowCustomEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_visualizer,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_refresh){
+            //web_eGallery.loadUrl("http://app.nivida.in/amulya_visualizer/");
+            web_eGallery.loadDataWithBaseURL("http://app.nivida.in/amulya_visualizer/",null,"text/html","UTF-8",null);
+        }
+
+        return true;
     }
 
     public class MyBrowser extends WebViewClient {
